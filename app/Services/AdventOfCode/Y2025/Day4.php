ecd20505d37a653a250b84f91d358f1c1a2751d1
Tom Mountain <tom.mountain@sykescottages.co.uk>
2025-12-04 16:25:51 +0000

    Day 4 step 2

--- app/Services/AdventOfCode/Y2025/Day4.php
+++ app/Services/AdventOfCode/Y2025/Day4.php
@@ -32,6 +32,33 @@ class Day4 extends BaseChallenge implements AdventChallenge
 
     public function handleStep2(string $input): mixed
     {
-        return "TODO: Implement step";
+        $total = 0;
+        $totalRemoved = 0;
+        $grid = $this->parseGrid($input);
+
+        while(true){
+            $removedThisLoop = 0;
+            foreach ($grid as $gridIndex => $line) {
+                foreach ($line as $lineIndex => $item) {
+                    if($item === '.') {
+                        continue;
+                    }
+                    $neighbours = $this->getNeighbors($grid, $gridIndex, $lineIndex);
+                    $filteredNeighbours = array_filter($neighbours, fn($n) => $n !== '.');
+                    if (count($filteredNeighbours) < 4) {
+                        // We can remove it
+                        $grid[$gridIndex][$lineIndex] = '.';
+                        $removedThisLoop++;
+                    }
+                }
+            }
+            if($removedThisLoop === 0){
+                break;
+            }
+
+            $totalRemoved = $totalRemoved + $removedThisLoop;
+        }
+
+        return $totalRemoved;
     }
 }
